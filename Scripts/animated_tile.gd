e74ee280856dcef00a3d23bb4952d29127cac658
TakeLime <jesper.eggers@gmail.com>
2021-04-24 21:23:28 +0200

    improtant fixes

--- Scripts/animated_tile.gd
+++ Scripts/animated_tile.gd
@@ -8,19 +8,21 @@ func _ready():
 	signals.connect("pause_mode_changed_to", self, "_on_pause_mode_changed_to")
 	
 	# Setup animation
+	var idle_animation: Animation = Animation.new()
 	var max_frame: int = hframes * vframes
-	$animation_player.get_animation("idle").length = float(str(max_frame)) / float(fps)
-	$animation_player.get_animation("idle").step = 1.0 / fps
-	$animation_player.get_animation("idle").loop = true
+	idle_animation.length = float(str(max_frame)) / float(fps)
+	idle_animation.step = 1.0 / fps
+	idle_animation.loop = true
 	
-	var anim_key: int = $animation_player.get_animation("idle").add_track(Animation.TYPE_BEZIER, -1)
-	$animation_player.get_animation("idle").track_set_path(anim_key, ":frame")
+	var anim_key: int = idle_animation.add_track(Animation.TYPE_BEZIER, -1)
+	idle_animation.track_set_path(anim_key, ":frame")
 
-	$animation_player.get_animation("idle").bezier_track_insert_key(anim_key, 0.0, 0)
+	idle_animation.bezier_track_insert_key(anim_key, 0.0, 0)
 	#$animation_player.get_animation("idle").bezier_track_insert_key(anim_key, 1.0 / float(fps) * (max_frame - 1), 3)
-	$animation_player.get_animation("idle").bezier_track_insert_key(anim_key, (1.0 / fps) * (max_frame - 1), max_frame - 1)
+	idle_animation.bezier_track_insert_key(anim_key, (1.0 / fps) * (max_frame - 1), max_frame - 1)
 	
 	# Start animation
+	$animation_player.add_animation("idle", idle_animation)
 	get_node("animation_player").play("idle")
 	
 
