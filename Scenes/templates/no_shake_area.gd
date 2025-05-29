09dd6dc16d6ff9daf7896253a802856055d1d847
TakeLime <jesper.eggers@gmail.com>
2025-05-29 19:07:05 +0200

    merged customizations

--- Scenes/templates/no_shake_area.gd
+++ Scenes/templates/no_shake_area.gd
@@ -2,14 +2,14 @@ class_name NoShakeZone
 extends Area2D
 
 func _ready():
-  assert(connect("area_entered",self,"_on_NoShakeZone_area_entered") == OK)
-  assert(connect("area_exited",self,"_on_NoShakeZone_area_exited") == OK)
+	assert(connect("area_entered",self,"_on_NoShakeZone_area_entered") == OK)
+	assert(connect("area_exited",self,"_on_NoShakeZone_area_exited") == OK)
 
 func _on_NoShakeZone_area_entered(area):
-  if area is Player:
-    paths.camera.no_shake_zone = true
+	if area is Player:
+		paths.camera.no_shake_zone = true
 
 
 func _on_NoShakeZone_area_exited(area):
-  if area is Player:
-    paths.camera.no_shake_zone = false
+	if area is Player:
+		paths.camera.no_shake_zone = false
